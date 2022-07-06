fa0bf3a2fd01a08f758399ec0c222f478f040a38
Servbt <arianrezvani00@gmail.com>
2022-07-06 01:15:09 -0400

    added styling for side search panel, need to work on functionality now

--- Assets/script.js
+++ Assets/script.js
@@ -3,16 +3,45 @@ console.log("testing");
 
 
 
-// this challenge seems to be divided into three main chunks //
+// this challenge seems to be divided into four main chunks //
 
 
-//a submission portion that finds the city your looking for
+// I need to successfully access the weather api and fectch the appropriate data //
+    // I'm probably going to need json
+    // once I get that I can pass that info (arguements) into other functions to use
+    // if I cant fetch I should probably try and display a message of some kind 
+
+//a submission portion that finds the city your looking for //
     // needs to include a submission form
-    // 
+        
+
+    // I also need a button to trigger submission form 
+        //need to make a function to search the data and find that once the button is clicked
+
+    //once that happens I need it to display the chosen city as a button underneath
+        // once the data is found  I need to make the proper elements 
+            //once the elements are made I need to set their respective text contents
+            // after that I have to also assign their proper tags for styling
+            // and finally I can append them on the page
+            
 
 
-//a status container that displays the current forecast of a city at the forefront
+
+
+//a status container that displays the current forecast of a city at the forefront //
+    //needs to add a header with a respective title 
+
+    //need to append the four proper pieces of info with the data that was parsed
+        // these are temperature, wind speed, humidity levels, and Ultraviolet index
+            //for UV index i need a way to style it if the number reacches a certain threshold
+
 
 
 //and finally a five day forecast that consists of cards that have some basic info attached to them
+    //similar to the previous card
+        // this time it seems to be looping through 5 independent pieces of iterations
+
+    // i'm gonna have a hard time with this..
+        //once I find a loop and get the appropriate data through each iteration I have to display that info in 5 cute little divs
+
     
