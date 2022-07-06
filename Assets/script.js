29144fd5b693f0ca7256bf00407a8c39a8368bd2
Servbt <arianrezvani00@gmail.com>
2022-07-06 00:17:50 -0400

    made header and also made a wireframe of website

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,18 @@
 console.log("testing");
+
+
+
+
+// this challenge seems to be divided into three main chunks //
+
+
+//a submission portion that finds the city your looking for
+    // needs to include a submission form
+    // 
+
+
+//a status container that displays the current forecast of a city at the forefront
+
+
+//and finally a five day forecast that consists of cards that have some basic info attached to them
+    
