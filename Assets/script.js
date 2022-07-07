ead2685380011dd028765cadfbb151489ffe7798
Servbt <arianrezvani00@gmail.com>
2022-07-07 00:29:11 -0400

    made call requests to both the weather and geopositional APIs

--- Assets/script.js
+++ Assets/script.js
@@ -4,45 +4,84 @@ console.log("testing if hooked up to js");
 
 
 // this challenge seems to be divided into four main chunks //
-    // i technically have to style the page too...
+// i technically have to style the page too...
 
-// I need to successfully access the weather api and fectch the appropriate data //
-    // I'm probably going to need json
-    // once I get that I can pass that info (arguements) into other functions to use
-        // I have to fetch location data
-        // I also have to fetch weather data based on location...
+// I need to successfully access the weather api and fetch the appropriate data //
+// I'm probably going to need json ~
+// once I get that I can pass that info (arguements) into other functions to use
+// I have to fetch location data
+// I also have to fetch weather data based on location...
 
-    // if I cant fetch I should probably try and display a message of some kind 
+// if I cant fetch I should probably try and display a message of some kind 
 
 //a submission portion that finds the city your looking for //
-    // needs to include a submission form
-        
+// needs to include a submission form
 
-    // I also need a button to trigger submission form 
-        //need to make a function to search the data and find that once the button is clicked
 
-    //once that happens I need it to display the chosen city as a button underneath
-        // once the data is found  I need to make the proper elements 
-            //once the elements are made I need to set their respective text contents
-            // after that I have to also assign their proper tags for styling
-            // and finally I can append them on the page
-            
+// I also need a button to trigger submission form 
+//need to make a function to search the data and find that once the button is clicked
+
+//once that happens I need it to display the chosen city as a button underneath
+// once the data is found  I need to make the proper elements 
+//once the elements are made I need to set their respective text contents
+// after that I have to also assign their proper tags for styling
+// and finally I can append them on the page
+
 
 //a status container that displays the current forecast of a city at the forefront //
-    //needs to add a header with a respective title 
+//needs to add a header with a respective title 
 
-    //need to append the four proper pieces of info with the data that was parsed
-        // these are temperature, wind speed, humidity levels, and Ultraviolet index
-            //for UV index i need a way to style it if the number reacches a certain threshold
+//need to append the four proper pieces of info with the data that was parsed
+// these are temperature, wind speed, humidity levels, and Ultraviolet index
+//for UV index i need a way to style it if the number reacches a certain threshold
 
 
 
 //and finally a five day forecast that consists of cards that have some basic info attached to them //
-    //similar to the previous card
-        // this time it seems to be looping through 5 independent pieces of iterations
-        //  I SHOULD PROBABLY MAKE RENDERING ALL OF THE DATA ONE FUNCTION (thanks to seths pseudo code)
+//similar to the previous card
+// this time it seems to be looping through 5 independent pieces of iterations
+//  I SHOULD PROBABLY MAKE RENDERING ALL OF THE DATA ONE FUNCTION (thanks to seths pseudo code)
 
-    // i'm gonna have a hard time with this..
-        //once I find a loop and get the appropriate data through each iteration I have to display that info in 5 cute little divs
+// i'm gonna have a hard time with this..
+//once I find a loop and get the appropriate data through each iteration I have to display that info in 5 cute little divs
 
-    
+
+
+
+
+
+var getlocationRepo = function () {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=orlando&appid=ca15c61b9bcc2642e628c234d0282a55";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                })
+            }
+        })
+}
+
+
+
+
+var getweatherRepo = function () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=ca15c61b9bcc2642e628c234d0282a55";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    console.log(data.current);
+                })
+            }
+        })
+}
+
+
+getweatherRepo();
+getlocationRepo();
